ab55c4a71d3b6188d6efe305eea7b7dce5280ccb
Aluno a27923 Teste <a27923@iMac37.local>
2026-05-13 11:56:21 +0100

    initial commit I LOVE SWIFT

---
+++ TaylorSwiftSentAHitmanMyWayUITests/TaylorSwiftSentAHitmanMyWayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TaylorSwiftSentAHitmanMyWayUITestsLaunchTests.swift
+//  TaylorSwiftSentAHitmanMyWayUITests
+//
+//  Created by Aluno a27923 Teste on 13/05/2026.
+//
+
+import XCTest
+
+final class TaylorSwiftSentAHitmanMyWayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
